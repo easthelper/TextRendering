e34e6a2d5a801b5513399ea8cd5977fa2d85db57
easthelper <easthelper@gmail.com>
2016-07-08 11:34:10 +0900

    일차 정리

--- TextRendering/ViewController.m
+++ TextRendering/ViewController.m
@@ -8,14 +8,18 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    int _bufferWidth;
+    int _bufferHeight;
+}
+
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
-CGSize g_renderedTextSize;
-CGSize g_finalRenderedSize;
+const NSUInteger bytesPerPixel = 4;
+const NSUInteger bitsPerComponent = 8;
 
 @implementation ViewController
 
@@ -27,17 +31,14 @@ CGSize g_finalRenderedSize;
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    int fontSize = 20;
-    const char * text = "안녕하새오; 고생이 만아오\nhello world\nhello world\n";
+    int fontSize = 30;
+    const char * text = "King of Ping Pong\nKing of Ping Pong";
     float scale = self.scale;
     
-    UIImage *image = [self IPhoneDrawTextToBufferAtX:0 y:0 fontSize:fontSize a:255 r:255 g:0 b:0 strokeWidth:10 sa:255 sr:255 sg:255 sb:255 msg:text];
-//    unsigned char *buffer = NULL;
-//    [self drawTextToBufferAtX:0 y:0 fontSize:fontSize a:255 r:255 g:0 b:0 strokeWidth:10 sa:255 sr:255 sg:255 sb:255 msg:text buffer:&buffer];
-    
+    UIImage *image = [self IPhoneDrawTextToBufferAtX:0 y:0 fontSize:fontSize a:255 r:255 g:0 b:0 strokeWidth:5 sa:255 sr:255 sg:255 sb:255 msg:text];
     
     self.imageView.image = image;
-    self.imageView.frame = CGRectMake(0, 0, g_finalRenderedSize.width/scale, g_finalRenderedSize.height/scale);
+    self.imageView.frame = CGRectMake(0, 0, _bufferWidth/scale, _bufferHeight/scale);
     
     self.label.font = [UIFont systemFontOfSize:fontSize];
     self.label.numberOfLines = 0;
@@ -63,6 +64,7 @@ CGSize g_finalRenderedSize;
                               sb:(int)sb
                              msg:(const char *)msg
 {
+    // 스케일은 여기서 담당
     float scale = self.scale;
     NSString *str = [NSString stringWithUTF8String:msg];
     
@@ -77,57 +79,78 @@ CGSize g_finalRenderedSize;
                                             blue:sb/255.f
                                            alpha:sa/255.f];
     // handle scale
-    UIFont *font = [UIFont systemFontOfSize:fontSize * scale];
-    strokeWidth *= scale;
+    UIFont *scaledfont = [UIFont systemFontOfSize:fontSize * scale];
+    float scaledStrokeWidth = strokeWidth * scale;
     
     NSDictionary *strokeAttribute =
     @{
-      NSFontAttributeName: font,
+      NSFontAttributeName: scaledfont,
       NSForegroundColorAttributeName: strokeColor,
-      NSStrokeWidthAttributeName: @(strokeWidth)
+      NSStrokeWidthAttributeName: @(scaledStrokeWidth)
       };
     
     NSDictionary *foregroundAttribute =
     @{
-      NSFontAttributeName: font,
+      NSFontAttributeName: scaledfont,
       NSForegroundColorAttributeName: foregroundColor,
       };
     
-    CGSize textSize = [str sizeWithAttributes: strokeAttribute];
-    int textWidth = textSize.width + strokeWidth * 2;
-    int textHeight = textSize.height + strokeWidth * 2;
+    CGSize scaledTextSize = [str sizeWithAttributes: strokeAttribute];
     
-    g_renderedTextSize = CGSizeMake(textWidth, textHeight);
-    NSLog(@"g_renderedTextSize %@", NSStringFromCGSize(g_renderedTextSize));
-    CGSize size = g_renderedTextSize;
-    // UIGraphicsBeginImageContext(size);
-    // UIGraphicsBeginImageContextWithOptions (size, false, 2.0f);
+    // calculate pixelbased size
+    int textWidth = scaledTextSize.width + scaledStrokeWidth;
+    int textHeight = scaledTextSize.height + scaledStrokeWidth;
     
-    int bufferWidth = MIN(size.width, 512 * scale);
-    int bufferHeight = MIN(size.height, 512 * scale);
+    CGSize textPixelSize = CGSizeMake(textWidth, textHeight);
+    NSLog(@"g_renderedTextSize %@", NSStringFromCGSize(textPixelSize));
     
-    g_finalRenderedSize = CGSizeMake(bufferWidth, bufferHeight);
+    // clipping buffer size
+    _bufferWidth = MIN(textPixelSize.width, 512 * scale);
+    _bufferHeight = MIN(textPixelSize.height, 512 * scale);
+
+    unsigned char *rawData = (unsigned char*) calloc(self.bufferLength, sizeof(unsigned char));
     
-    NSUInteger bytesPerPixel = 4;
-    NSUInteger bytesPerRow = bytesPerPixel * bufferWidth;
-    NSUInteger bitsPerComponent = 8;
+    [self drawTextToBufferAtX:0 y:0
+              strokeAttribute:strokeAttribute
+          foregroundAttribute:foregroundAttribute
+                  strokeWidth:scaledStrokeWidth
+                          msg:str
+                       buffer:rawData];
+    
+    // create image from rawdata
+    UIImage *image = [self imageFromBuffer:rawData
+                               bufferWidth:_bufferWidth
+                              bufferHeight:_bufferHeight];
+    
+    free(rawData);
+    
+    return image;
+}
+
+-(void)drawTextToBufferAtX:(int)x
+                         y:(int)y
+           strokeAttribute:(NSDictionary *)strokeAttribute
+       foregroundAttribute:(NSDictionary *)foregroundAttribute
+               strokeWidth:(float)strokeWidth
+                       msg:(NSString *)str
+                    buffer:(unsigned char *)buffer
+{
+    NSUInteger bytesPerRow = bytesPerPixel * _bufferWidth;
     
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    NSUInteger bufferLength = bufferWidth * bufferHeight * bytesPerPixel;
-    unsigned char *rawData = (unsigned char*) calloc(bufferLength, sizeof(unsigned char));
     
     CGContextRef context = CGBitmapContextCreate(
-                                                 rawData,
-                                                 bufferWidth,
-                                                 bufferHeight,
+                                                 buffer,
+                                                 _bufferWidth,
+                                                 _bufferHeight,
                                                  bitsPerComponent,
                                                  bytesPerRow,
                                                  colorSpace,
-                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+                                                 self.bitmapInfo);
     
     // CGContextSetAllowsAntialiasing( context, false );
     // CGContextSetInterpolationQuality( context, kCGInterpolationHigh );
-    CGContextTranslateCTM(context, 0.0f, (bufferHeight - strokeWidth) );
+    CGContextTranslateCTM(context, 0.0f, (_bufferHeight - strokeWidth / 2) );
     CGContextScaleCTM(context, 1.0f, -1.0f); //NOTE: NSString draws in UIKit referential i.e. renders upside-down compared to CGBitmapContext referential
     UIGraphicsPushContext(context);
     
@@ -135,153 +158,63 @@ CGSize g_finalRenderedSize;
     CGContextSetShouldSubpixelPositionFonts(context, false);
 //    CGContextSetShouldAntialias( context, false );
     // CGContextSetTextDrawingMode(context, kCGTextStroke);
-    [str drawInRect:CGRectMake(0 + strokeWidth, 0 + strokeWidth, 10000, 512) withAttributes: strokeAttribute];
+    [str drawInRect:CGRectMake(0 + strokeWidth / 2, 0 + strokeWidth / 2, 10000, 512 * self.scale) withAttributes: strokeAttribute];
     
     // CGContextSetTextDrawingMode(context, kCGTextFill);
-    [str drawInRect:CGRectMake(0 + strokeWidth, 0 + strokeWidth, 10000, 512) withAttributes: foregroundAttribute];
-    
-    // UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    // NSLog(@"image %@", NSStringFromCGSize(image.size));
-    // NSLog(@"image %d", CGImageGetWidth(image.CGImage));
+    [str drawInRect:CGRectMake(0 + strokeWidth / 2, 0 + strokeWidth / 2, 10000, 512 * self.scale) withAttributes: foregroundAttribute];
     
     UIGraphicsPopContext();
     CGContextRelease(context);
     
+    CGColorSpaceRelease(colorSpace);
+}
+
+- (UIImage *)imageFromBuffer:(unsigned char *)buffer
+                 bufferWidth:(NSUInteger)bufferWidth
+                bufferHeight:(NSUInteger)bufferHeight {
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    // create cgimage from rawdata
     CGDataProviderRef provider = CGDataProviderCreateWithData(NULL,
-                                                              rawData,
-                                                              bufferLength,
+                                                              buffer,
+                                                              self.bufferLength,
                                                               NULL);
     
-    
-    CGBitmapInfo bitmapInfo = kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big;
     CGColorRenderingIntent renderingIntent = kCGRenderingIntentDefault;
-    CGImageRef imageRef = CGImageCreate(bufferWidth,
-                                        bufferHeight,
+    CGImageRef imageRef = CGImageCreate(_bufferWidth,
+                                        _bufferHeight,
                                         bitsPerComponent,
                                         32,
-                                        4*bufferWidth,
+                                        4*_bufferWidth,
                                         colorSpace,
-                                        bitmapInfo,
+                                        self.bitmapInfo,
                                         provider,
                                         NULL,
                                         NO,
                                         renderingIntent);
+    
+    CGColorSpaceRelease(colorSpace);
+    
     /*I get the current dimensions displayed here */
     NSLog(@"width=%zu, height: %zu", CGImageGetWidth(imageRef),
           CGImageGetHeight(imageRef) );
-//    UIImage *image = [UIImage imageWithCGImage:imageRef];
-    UIImage *image = [UIImage imageWithCGImage:imageRef scale:scale orientation:0];
     
-    free(rawData);
-    CGColorSpaceRelease(colorSpace);
-    
+    UIImage *image = [UIImage imageWithCGImage:imageRef scale:self.scale orientation:0];
     return image;
 }
 
--(void)drawTextToBufferAtX:(int)x
-                                    y:(int)y
-                             fontSize:(int)fontSize
-                                    a:(int)a
-                                    r:(int)r
-                                    g:(int)g
-                                    b:(int)b
-                          strokeWidth:(float)strokeWidth
-                                   sa:(int)sa
-                                   sr:(int)sr
-                                   sg:(int)sg
-                                   sb:(int)sb
-                                  msg:(const char *)msg
-                    buffer:(unsigned char **)buffer
-{
-    float scale = self.scale;
-    NSString *str = [NSString stringWithUTF8String:msg];
-    
-    NSLog(@"%d,%d,%d %d,%d,%d,%d %f, %d,%d,%d,%d %@ ", x,y,fontSize,a,r,g,b,strokeWidth,sa,sr,sg,sb,str);
-    
-    UIColor *foregroundColor = [UIColor colorWithRed:r/255.f
-                                               green:g/255.f
-                                                blue:b/255.f
-                                               alpha:a/255.f];
-    UIColor *strokeColor = [UIColor colorWithRed:sr/255.f
-                                           green:sg/255.f
-                                            blue:sb/255.f
-                                           alpha:sa/255.f];
-    // handle scale
-    UIFont *font = [UIFont systemFontOfSize:fontSize * scale];
-    strokeWidth *= scale;
-    
-    NSDictionary *strokeAttribute =
-    @{
-      NSFontAttributeName: font,
-      NSForegroundColorAttributeName: strokeColor,
-      NSStrokeWidthAttributeName: @(strokeWidth)
-      };
-    
-    NSDictionary *foregroundAttribute =
-    @{
-      NSFontAttributeName: font,
-      NSForegroundColorAttributeName: foregroundColor,
-      };
-    
-    CGSize textSize = [str sizeWithAttributes: strokeAttribute];
-    int textWidth = textSize.width + strokeWidth * 2;
-    int textHeight = textSize.height + strokeWidth * 2;
-    
-    g_renderedTextSize = CGSizeMake(textWidth, textHeight);
-    NSLog(@"g_renderedTextSize %@", NSStringFromCGSize(g_renderedTextSize));
-    CGSize size = g_renderedTextSize;
-    
-    int bufferWidth = MIN(size.width, 512 * scale);
-    int bufferHeight = MIN(size.height, 512 * scale);
-    
-    g_finalRenderedSize = CGSizeMake(bufferWidth, bufferHeight);
-    
-    NSUInteger bytesPerPixel = 4;
-    NSUInteger bytesPerRow = bytesPerPixel * bufferWidth;
-    NSUInteger bitsPerComponent = 8;
-    
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    NSUInteger bufferLength = bufferWidth * bufferHeight * bytesPerPixel;
-    *buffer = (unsigned char*) calloc(bufferLength, sizeof(unsigned char));
-    
-    CGContextRef context = CGBitmapContextCreate(
-                                                 buffer,
-                                                 bufferWidth,
-                                                 bufferHeight,
-                                                 bitsPerComponent,
-                                                 bytesPerRow,
-                                                 colorSpace,
-                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
-    
-    // CGContextSetAllowsAntialiasing( context, false );
-    // CGContextSetInterpolationQuality( context, kCGInterpolationHigh );
-    CGContextTranslateCTM(context, 0.0f, (bufferHeight - strokeWidth) );
-    CGContextScaleCTM(context, 1.0f, -1.0f); //NOTE: NSString draws in UIKit referential i.e. renders upside-down compared to CGBitmapContext referential
-    UIGraphicsPushContext(context);
-    
-    CGContextSetShouldSubpixelQuantizeFonts(context, false);
-    CGContextSetShouldSubpixelPositionFonts(context, false);
-    //    CGContextSetShouldAntialias( context, false );
-    // CGContextSetTextDrawingMode(context, kCGTextStroke);
-    [str drawInRect:CGRectMake(0 + strokeWidth, 0 + strokeWidth, 10000, 512) withAttributes: strokeAttribute];
-    
-    // CGContextSetTextDrawingMode(context, kCGTextFill);
-    [str drawInRect:CGRectMake(0 + strokeWidth, 0 + strokeWidth, 10000, 512) withAttributes: foregroundAttribute];
-    
-    UIGraphicsPopContext();
-    CGContextRelease(context);
-    
-    
-//    free(rawData);
-    CGColorSpaceRelease(colorSpace);
-    
-//    return image;
+- (CGBitmapInfo)bitmapInfo {
+    return kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big;
 }
 
+- (NSUInteger)bufferLength {
+    return _bufferWidth * _bufferHeight * bytesPerPixel;
+}
 
 - (float)scale {
-    return [UIScreen mainScreen].scale;
-//    return 1;
+//    return [UIScreen mainScreen].scale;
+    return 2;
 }
 
 @end
